064f5b7cbd4a296cc3babba313992f5cfbeac02f
Varun Panwar <varunpanwar4321@gmail.com>
2024-09-21 08:16:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <><nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -13,10 +12,7 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">About Us</Link>
+          <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
         </li>
       </ul>
       <div className={`custom-control custom-switch text-${props.mode==='light'?'dark':'light'}`}>
